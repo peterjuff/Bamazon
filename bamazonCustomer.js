495781b990f1d8181036f4ffa0a80944f878b712
Peter Juffernbruch <peterjuffernbruch@gmail.com>
2018-02-18 19:37:05 -0600

    Customer view success

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -13,7 +13,7 @@ var connection = mysql.createConnection({
 // connect to the mysql server and sql database
 connection.connect(function(err) {
   if (err) throw err;
-  console.log("connected as id " + connection.threadId + "\n");
+  //console.log("connected as id " + connection.threadId + "\n");
   listProducts();
   
 });
@@ -21,8 +21,49 @@ connection.connect(function(err) {
 function listProducts() {
    connection.query("select * from products", function(err, res) {
        for(var i = 0; i < res.length; i++) {
-           console.log(res[i].id + " | " + res[i].product + " | " + res[i].department + " | " + res[i].price + " | " + res[i].stock);
+           console.log("\n\t" + "ID " + res[i].id + " | " + res[i].product + " | "  + res[i].price);
+
        }
+       userPrompt();
+       
    })
-    //console.log("works");
 };
+function userPrompt() {
+inquirer.prompt([
+    
+    {
+        type: "input",
+        name: "userId",
+        message: "What is the ID of the product you would like to buy?"
+    },
+
+    {
+        type: "input",
+        name: "userQuantity",
+        message: "How many units would you like to buy?"
+    }
+
+
+   ]).then(function(response) {
+       var id = response.userId;
+       var quantity = response.userQuantity;
+       connection.query("select stock, price from products where id = ? ", [id], function(err, res) {
+        
+        for (var j = 0; j < res.length; j++) {
+            var stockDB = res[j].stock;
+            var priceDB = res[j].price;
+        }
+        if (quantity <= stockDB) {
+            connection.query("update products set stock = stock - ? where id = ? ", [quantity, id], function(err2, res2) {
+            var total = priceDB * quantity;
+            console.log("Your total is: " + total);
+        })
+
+
+        }
+        else {console.log("insufficient stock");}
+       })
+   })
+
+
+}
